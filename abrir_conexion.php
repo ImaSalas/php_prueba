2cf54b28ec76905cbd7da841664dff153c8021d8
ImaSalas <92271130+ImaSalas@users.noreply.github.com>
2022-05-15 14:33:46 -0500

    Update abrir_conexion.php

--- abrir_conexion.php
+++ abrir_conexion.php
@@ -1,9 +1,9 @@
 <?php 
 	// Parametros a configurar para la conexion de la base de datos 
-	$host = "localhost";    // sera el valor de nuestra BD 
-	$basededatos = "propietario";    // sera el valor de nuestra BD 
-	$usuariodb = "dostin";    // sera el valor de nuestra BD 
-	$clavedb = "12345";    // sera el valor de nuestra BD 
+	$host = "20.25.51.254";    // sera el valor de nuestra BD 
+	$basededatos = "newAzure";    // sera el valor de nuestra BD 
+	$usuariodb = "server";    // sera el valor de nuestra BD 
+	$clavedb = "123456789";    // sera el valor de nuestra BD 
 
 	//Lista de Tablas
 	$tabla_db1 = "propietario"; 	   // tabla de usuarios
